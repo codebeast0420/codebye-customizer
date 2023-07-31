fdb7abd91fb332134ced917ec74c712db0375415
John <codebeast0420@gmail.com>
2023-07-30 21:32:53 -0400

    add firebase for aquafiore

--- src/components/common/Header/Header.js
+++ src/components/common/Header/Header.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import Logo from './logo.svg'
-import LHLogo from '../../../assets/images/lh-logo.svg';
-import APLogo from '../../../assets/images/ap-logo.svg';
+import LHLogo from './lh-logo.svg';
+import APLogo from './ap-logo.svg';
 import ChatLogo from './chat.svg'
 import Pricing from '../../../lib/pricing';
 import { useSelector } from 'react-redux';
@@ -9,7 +9,7 @@ import { store } from '../../../lib/env';
 import axios from 'axios';
 import imageCompression from 'browser-image-compression';
 import { URL, client } from '../../../lib/env'
-import { collection, query, orderBy, onSnapshot, addDoc } from "firebase/firestore"
+import { collection, query, orderBy, onSnapshot } from "firebase/firestore"
 import { firebaseDB } from '../../../lib/firebase';
 import { db } from '../SettingMenu/db';
 import DomToImage from 'dom-to-image';
@@ -40,6 +40,7 @@ const Header = ({ showContactModal, message }) => {
   }, []);
 
   useEffect(() => {
+    console.log('changed', product.data.id);
     let priceDB = [];
     let material = configuration.pa_material.name;
     if (material == "18kt Rose Gold") material = "18ct Recycled Rose Gold";
@@ -101,81 +102,80 @@ const Header = ({ showContactModal, message }) => {
         })
       }
       else {
-        let tempPrice = db.filter((d) => d.id == product.data.id)[0].options;
-        console.log('tempri', tempPrice);
-        let temp2 = tempPrice.filter((opt) => opt.text == material)[0];
-        console.log('temp2', temp2);
-        let total = 0;
-        configuration.message.split("").map((e) => total += temp2.letters[e.toUpperCase()]);
-
-        const currentUrl = window.location.href;
-        console.log('Current URL:', currentUrl, typeof (currentUrl));
-        const lister = "lister";
-        const anjapotze = "anjapotze";
-        if (currentUrl.includes(lister)) {
-          setSubLogo("lister");
+        if (product.data.id == 408) {
+          q = query(collection(firebaseDB, 'aquafiore-earrings'))
         }
-        if (currentUrl.includes(anjapotze)) {
-          setSubLogo("anjapotze");
+        if (product.data.id == 402) {
+          q = query(collection(firebaseDB, 'aquafiore-bracelet'))
         }
-        setPrice(total);
-        setIsDisabled(false);
+        if (product.data.id == 405) {
+          q = query(collection(firebaseDB, 'aquafiore-pendant'))
+        }
+        onSnapshot(q, async (querySnapshot) => {
+          console.log('query', querySnapshot.docs);
+          priceDB = querySnapshot.docs.filter(doc => (
+            doc.id == material
+          ))[0].data();
+          let total = 0;
+          configuration.message.split("").map((e) => {
+            total += priceDB[e.toUpperCase()];
+            console.log('total', total)
+          });
+          setPrice(total);
+          setIsDisabled(false)
+        })
       }
     }
   }, [configuration])
 
   const buy = async () => {
-    // const dbRef = collection(firebaseDB, "carts");
-    // const image = document.getElementById('getImage');
-    // const docId = "images";
-    // DomToImage.toJpeg(image, { quality: 0.95 }).then(async (dataUrl) => {
-    //   console.log('success', dataUrl);
-    //   const docRef = await addDoc(dbRef, docId, {image: dataUrl});
-    // })
+    const image = document.getElementById('getImage');
+    DomToImage.toJpeg(image, { quality: 0.95 }).then(async (dataUrl) => {
+      console.log('success', dataUrl);
+      axios.post("https://codeby-backend-tau.vercel.app/add-to-cart", { productId: productId }).then((res) => {
+        let variants = res.data.product?.variants;
+        let variant = "not_found";
+        variants.map((v) => {
+          if (parseInt(v.price) == parseInt(price)) {
+            variant = v;
+          }
+        });
+        console.log('variant', variant);
+        if (variant == "not_found") {
+          // create one
+          axios
+            .post("https://codeby-backend-tau.vercel.app/create-cart", {
+              productId: productId,
+              price: price,
+            })
+            .then(function (response) {
+              variant = response.data.variant;
+              window.location.assign(`https://codebyedge.co.uk/cart/add?id=${variant.id}&quantity=1&properties[message]=${encodeURIComponent(configuration.message)}&properties[finish]=${encodeURIComponent(configuration.pa_material.name)}&properties[size]=${encodeURIComponent(configuration.pa_size.name)}`, "");
 
-    axios.post("https://codeby-backend.vercel.app/add-to-cart", { productId: productId }).then((res) => {
-      let variants = res.data.product?.variants;
-      let variant = "not_found";
-      variants.map((v) => {
-        if (parseInt(v.price) == parseInt(price)) {
-          variant = v;
+            })
+            .catch(function (error) {
+              console.log(error);
+              console.log(error.response);
+            });
+        } else {
+          // ready to redirect
+          window.location.assign(`https://codebyedge.co.uk/cart/add?id=${variant.id}&quantity=1&properties[message]=${encodeURIComponent(configuration.message)}&properties[finish]=${encodeURIComponent(configuration.pa_material.name)}&properties[size]=${encodeURIComponent(configuration.pa_size.name)}`, "");
         }
-      });
-      console.log('variant', variant);
-      if (variant == "not_found") {
-        // create one
-        axios
-          .post("https://codeby-backend.vercel.app/create-cart", {
-            productId: productId,
-            price: price,
-          })
-          .then(function (response) {
-            variant = response.data.variant;
-            window.location.assign(`https://codebyedge.co.uk/cart/add?id=${variant.id}&quantity=1&properties[message]=${encodeURIComponent(configuration.message)}&properties[finish]=${encodeURIComponent(configuration.pa_material.name)}&properties[size]=${encodeURIComponent(configuration.pa_size.name)}`, "");
-
-          })
-          .catch(function (error) {
-            console.log(error);
-            console.log(error.response);
-          });
-      } else {
-        // ready to redirect
-        window.location.assign(`https://codebyedge.co.uk/cart/add?id=${variant.id}&quantity=1&properties[message]=${encodeURIComponent(configuration.message)}&properties[finish]=${encodeURIComponent(configuration.pa_material.name)}&properties[size]=${encodeURIComponent(configuration.pa_size.name)}`, "");
-      }
-      console.log(res);
+        console.log(res);
+      })
     })
   }
 
   return (
-    <div className="flex md:flex-row lg:flex-row header-height  flex-col-reverse content-center  gap-4 justify-center lg:mt-6" id='header'>
+    <div className="flex md:flex-row lg:flex-row header-height  flex-col-reverse content-center gap-4 justify-center lg:mt-6" id='header'>
       <div className="md:flex basis-4/12 justify-center items-center hidden">
         <div className="">
           <a role="button" tabIndex="0" href="https://codebyedge.co.uk" className="a a-link flex">
             <img src={Logo} alt="logo" srcSet="" width="100" />
-            {window.location.href.includes("lister") && (
+            {window.location.href.includes('lister') && (
               <img src={LHLogo} className="ml-2 lh-logo" alt="lh-logo" srcSet='' />
             )}
-            {window.location.href.includes("anjapotze") && (
+            {window.location.href.includes('anjapotze') && (
               <img src={APLogo} className="ml-3 ap-logo" alt="lh-logo" srcSet='' />
             )}
 
